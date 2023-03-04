eea071b1dc2d4b7d8d3b5228fd4f388524f15cfe
joshuaamarasinghe <joshuaamarasinghe@gmail.com>
2023-03-04 16:36:15 +0530

    update

--- MembershipRegisterSubscriber/src/com/mtit/MembershipRegisterSubscriber/MembershipSubscriberImpl.java
+++ MembershipRegisterSubscriber/src/com/mtit/MembershipRegisterSubscriber/MembershipSubscriberImpl.java
@@ -23,82 +23,79 @@ public class MembershipSubscriberImpl implements MembershipSubscriber {
 	
 	@Override
 	public void getService(MembershipPublisher membershippublisher) {
-	      System.out.println("=========== Welcome to the Admin Portal ============");
-	      System.out.println();
-	      System.out.println("Select the service you want. ");
-	      System.out.println();
-	      
-	      while(true) {
-	    	  input = getInput();
-	    	  
-	    	  if(input.equalsIgnoreCase(ADD)) {
-	    		  addRecord(membershippublisher);
-	    	  }
-	    	  else if (input.equalsIgnoreCase(All)) {
-	    		  getAllRecords(membershippublisher);
-	    	  }
-	    	  else if (input.equalsIgnoreCase(DELETE)) {
-	    		  deleteRecord(membershippublisher);
-	    	  }
-	    	  else {
-	    		  System.out.println("Invalid");
-	    	  }
-	      }
-		
+		System.out.println("=========== Welcome to the Membership Registration Portal ============");
+		System.out.println();
+		System.out.println("Select the service you want. ");
+		System.out.println();
+
+		while (true) {
+			input = getInput();
+
+			if (input.equalsIgnoreCase(ADD)) {
+				addRecord(membershippublisher);
+			} else if (input.equalsIgnoreCase(All)) {
+				getAllRecords(membershippublisher);
+			} else if (input.equalsIgnoreCase(DELETE)) {
+				deleteRecord(membershippublisher);
+			} else {
+				System.out.println("Invalid");
+			}
+		}
+
 	}
 
-public String getInput() {
-	   System.out.println("To Insert a record to the portal type: 'Add'");
-	   System.out.println("To View records from the portal type: 'All'");
-	   System.out.println("To Delete a record from the portal type: 'Delete'");
-	   System.out.println();
-	   String input =sc.nextLine();
-	   return input;
-	   
-   }
-   
-   public void addRecord(MembershipPublisher membershippublisher) {
-	   System.out.println("Enter Member ID:");
-	   member_ID = sc.nextLine();
-	   
-	   System.out.println("Enter Member's First Name:");
-	   fname = sc.nextLine();
-	   
-	   System.out.println("Enter Member's Last Name");
-	   lname = sc.nextLine();
-	   
-	   System.out.println("Enter Contact Number:");
-	   contact_no = sc.nextLine();
-	   
-	   System.out.println("Enter Membership Type:");
-	   mem_type = sc.nextLine();
-	   
-	   System.out.println("Enter Membership Valid for (Year): ");
-	   valid_years = sc.nextLine();
-	   
-	   String temp = membershippublisher.addMember(member_ID, fname, lname, contact_no, mem_type, valid_years);
-	   System.out.println(temp);
-	   System.out.println();
-   }
-   
-   public void getAllRecords(MembershipPublisher membershippublisher) {
-	   List<Membership> memberships = membershippublisher.getAllMembers();
-	   
-	   System.out.println("Displaying all Registered Members ");
-	   System.out.println();
-	   
-	   System.out.println("ID \t Full Name");
-	   for(Membership membership : memberships ) {
-		   
-		   System.out.println(membership.getmember_ID());
-		   System.out.println("\t" +  membership.getFname() + " " + membership.getLname());
-		   
-		   System.out.println();
-	   }
-	   
-	   System.out.println();
-   }
-   
+	public String getInput() {
+		System.out.println("Add Members: 'Add'");
+		System.out.println("View Member Details: 'All'");
+		System.out.println("Remove Members: 'Delete'");
+		System.out.println();
+		System.out.println("Type the prefered Selection: ");
+		String input = sc.nextLine();
+		return input;
+
+	}
+
+	public void addRecord(MembershipPublisher membershippublisher) {
+		System.out.println("Enter Member ID:");
+		member_ID = sc.nextLine();
+
+		System.out.println("Enter Member's First Name:");
+		fname = sc.nextLine();
+
+		System.out.println("Enter Member's Last Name");
+		lname = sc.nextLine();
+
+		System.out.println("Enter Contact Number:");
+		contact_no = sc.nextLine();
+
+		System.out.println("Enter Membership Type:");
+		mem_type = sc.nextLine();
+
+		System.out.println("Enter Membership Valid for (Year): ");
+		valid_years = sc.nextLine();
+
+		String temp = membershippublisher.addMember(member_ID, fname, lname, contact_no, mem_type, valid_years);
+		System.out.println(temp);
+		System.out.println();
+	}
+
+	public void getAllRecords(MembershipPublisher membershippublisher) {
+		List<Membership> memberships = membershippublisher.getAllMembers();
+
+		System.out.println("Displaying all Registered Members ");
+		System.out.println();
+
+		System.out.println("ID \t Full Name");
+		for (Membership membership : memberships) {
+
+			System.out.println(membership.getmember_ID());
+			System.out.println("\t" + membership.getFname() + " " + membership.getLname());
+
+			System.out.println();
+		}
+
+		System.out.println();
+	}
 
    private void deleteRecord(MembershipPublisher membershippublisher) {
 		String doc_id;
@@ -108,7 +105,5 @@ public String getInput() {
 		membershippublisher.deleteMember(doc_id);
 		System.out.println();
 	}
-   
-	
 
 }
