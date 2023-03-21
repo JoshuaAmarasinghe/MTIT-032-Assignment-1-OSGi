0af18e1d9762f63af28bd41887c6a27406f5ed9f
pawanlaksahan <pawanlaksahan10@gmail.com>
2023-03-21 13:49:11 +0530

    Yoga Publisher changes

--- YogaRegisterSubscriber/src/com/mtit/YogaSubscriber/YogaSubscriber.java
+++ YogaRegisterSubscriber/src/com/mtit/YogaSubscriber/YogaSubscriber.java
@@ -3,5 +3,5 @@ package com.mtit.YogaSubscriber;
 import com.mtit.YogaPublisher.YogaPublisher;
 
 public interface YogaSubscriber {
-         public void getService(YogaPublisher yogapublisher);
+         public void getService(YogaPublisher yoga publisher);
 }
